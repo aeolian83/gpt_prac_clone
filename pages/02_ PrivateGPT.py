1e84455660a0e116121f7657ffa81e6d171c6fd7
aeolian83 <aeolian83@gmail.com>
2024-01-11 16:13:56 +0900

    private gpt

--- pages/02_ PrivateGPT.py
+++ pages/02_ PrivateGPT.py
@@ -1,12 +1,9 @@
-from typing import Any, Dict, List, Optional, Union
-from uuid import UUID
-from langchain.schema.output import ChatGenerationChunk, GenerationChunk
 import streamlit as st
 
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import ChatOllama
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.embeddings import OllamaEmbeddings, CacheBackedEmbeddings
 
 # 벡터 스토어에 따라 성능이 달라지기도 한다.
 from langchain.vectorstores import FAISS
@@ -35,7 +32,12 @@ class ChatCallbackHandler(BaseCallbackHandler):
         self.message_box.markdown(self.message)
 
 
-llm = ChatOpenAI(
+# falcon model is not work
+with st.sidebar:
+    model = st.selectbox("Choose your model", ("mistral:latest", "falcon:latest"))
+
+llm = ChatOllama(
+    model=model,
     temperature=0.1,
     streaming=True,
     callbacks=[
@@ -47,10 +49,10 @@ llm = ChatOpenAI(
 @st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
+    file_path = f"./.cache/private_files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
-    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    cache_dir = LocalFileStore(f"./.cache/private_embeddings/{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
@@ -58,7 +60,7 @@ def embed_file(file):
     )
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings()
+    embeddings = OllamaEmbeddings(model=model)
     cache_embeddings = CacheBackedEmbeddings.from_bytes_store(
         embeddings,
         cache_dir,
@@ -76,7 +78,6 @@ def save_message(message, role):
 def send_message(message, role, save=True):
     with st.chat_message(role):
         st.markdown(message)
-
     if save:
         save_message(message, role)
 
@@ -90,19 +91,40 @@ def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-Answer the question using ONLY the followin context. If you don't know the answer
-just say you don't know. DON'T make anything up.
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         (
+#             "system",
+#             """
+# Answer the question using ONLY the followin context. If you don't know the answer
+# just say you don't know. DON'T make anything up.
+
+# Context: {context}
+# """,
+#         ),
+#         ("human", "{question}"),
+#     ]
+# )
+
+# st.title("DocumentGPT")
+
+# st.markdown(
+#     """
+# Welcome!
+
+# Use this chatbot to ask question to an AI about your files!
+
+# Upload your files on the sidebar.
+# """
+# )
+
+prompt = ChatPromptTemplate.from_template(
+    """Answer the question using ONLY the followin context and not your training data. 
+        If you don't know the answer just say you don't know. DON'T make anything up.
 
 Context: {context}
-""",
-        ),
-        ("human", "{question}"),
-    ]
+Question: {question}
+"""
 )
 
 st.title("DocumentGPT")
@@ -116,6 +138,7 @@ Use this chatbot to ask question to an AI about your files!
 Upload your files on the sidebar.
 """
 )
+
 
 with st.sidebar:
     file = st.file_uploader(
--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
+st.set_page_config(page_title="DcumentGPT", page_icon="")
+
 st.title("QuizGPT")
